fbd382d9cbcbaf736efc9b45c7276fa9ab0b92b2
FEI\jianfu.liu <jianfu.liu@feisystems.com>
2018-10-31 14:27:16 -0400

    Update CareVisit/CareVisit.Core/Helpers/IdentityServerResourceOwerAuthenticator.cs

--- CareVisit/CareVisit.Core/Helpers/IdentityServerResourceOwerAuthenticator.cs
+++ CareVisit/CareVisit.Core/Helpers/IdentityServerResourceOwerAuthenticator.cs
@@ -12,6 +12,14 @@ namespace EvvMobile.Authorization
 {
     public class IdentityServerResourceOwerAuthenticator
     {
+        public static string ClientId = "A8375B66";
+        public static string ClientSecret = "A32D8C3CBE9A";
+        public static string Scope = "openid offline_access";//offline_access is for Identity Server, to get refresh token
+        public static string ResponseType = "code id_token token";
+        public static string AuthorizeUrl = "https://jianfuliult4.fei.local/Identity/connect/authorize/";//
+        public static string AccessTokenUrl = "https://jianfuliult4.fei.local/Identity/connect/token";
+        public static string UserInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+        public static string RedirectUrl = "https://jianfuliult4.fei.local/Identity/connect/authorize/";
 
         public IdentityServerResourceOwerAuthenticator(string clientId, string clientSecret, string scope, GetUsernameAsyncFunc getUsernameAsync = null)
         {
